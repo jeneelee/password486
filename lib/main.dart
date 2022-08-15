6b4fa31b2dbb909bd25ff20033312017ceaa5d3c
jeneelee <won5498@gmail.com>
2022-08-16 00:11:11 +0900

    5 commit

--- lib/main.dart
+++ lib/main.dart
@@ -19,22 +19,60 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class PassWord486 extends StatelessWidget {
+class PassWord486 extends StatefulWidget {
   const PassWord486({Key? key}) : super(key: key);
 
   @override
+  State<PassWord486> createState() => _PassWord486State();
+}
+
+class _PassWord486State extends State<PassWord486> {
+  final List<String> entries = [];
+  final textController = TextEditingController();
+
+  @override
   Widget build(BuildContext context) {
+    entries.sort();
+
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Password486'),
-          centerTitle: true,
+      appBar: AppBar(
+        //title: Text('Password486'),
+        title: TextField(
+          controller: textController,
         ),
-        body: ListView(
-          children: [
-            Text('A'),
-            Text('B'),
-            Text('C'),
-          ],
-        ));
+      ),
+      body: entries.length > 0
+          ? ListView.separated(
+              itemCount: entries.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  leading: Icon(Icons.local_grocery_store),
+                  title: Text('${entries[index]}'),
+                  onLongPress: () {
+                    setState(() {
+                      entries.removeAt(index);
+                    });
+                  },
+                  trailing: Icon(Icons.remove_circle_outline),
+                );
+              },
+              separatorBuilder: (BuildContext context, int index) =>
+                  const Divider(
+                color: Colors.black,
+              ),
+            )
+          : Center(
+              child: Text('No groceries in your list.'),
+            ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.save),
+        onPressed: () {
+          setState(() {
+            entries.add(textController.text);
+            textController.clear();
+          });
+        },
+      ),
+    );
   }
 }
